a5e33704eec312b1b94ea3ec46115ba650d47c64
leobaz <leutrim.kosumi@webtiser.ch>
2020-08-27 14:16:19 +0200

    header component, bootstrap and font-awesome, weather display

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { WeatherDisplayComponent } from './weather-display/weather-display.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: WeatherDisplayComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/weather-display/weather-display.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-weather-display',
+  templateUrl: './weather-display.component.html',
+  styleUrls: ['./weather-display.component.scss']
+})
+export class WeatherDisplayComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
