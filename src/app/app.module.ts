ae13f24db4cfb2daba97329715a5696a0eb42422
leobaz <leutrim.kosumi@webtiser.ch>
2020-08-28 15:53:56 +0200

    add chart display

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { WeatherDisplayComponent } from './weather-display/weather-display.component';
+import { WeatherChartComponent } from './weather-chart/weather-chart.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    WeatherDisplayComponent
+    WeatherDisplayComponent,
+    WeatherChartComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
