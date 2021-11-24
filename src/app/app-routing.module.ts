215a6a93497e580381d6b3d33881b9fee46f5abb
leobaz <leutrim.kosumi@hotmail.com>
2021-11-24 11:25:06 +0100

    update angular to version 11

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,7 @@ import { WeatherComponent } from "./components/weather/weather.component";
 const routes: Routes = [{ path: "", component: WeatherComponent }];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { relativeLinkResolution: 'legacy' })],
   exports: [RouterModule],
 })
 export class AppRoutingModule {}
--- src/app/components/weather/weather-chart/weather-chart.component.spec.ts
+++ src/app/components/weather/weather-chart/weather-chart.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { WeatherChartComponent } from './weather-chart.component';
 
@@ -6,7 +6,7 @@ describe('WeatherChartComponent', () => {
   let component: WeatherChartComponent;
   let fixture: ComponentFixture<WeatherChartComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ WeatherChartComponent ]
     })
